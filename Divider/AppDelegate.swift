aecbb4c89c7d0affb1512bcb97b591710815c71c
Jake Convery <jakeconv@gmail.com>
2020-09-14 17:45:26 -0400

    Initial Commit

---
+++ Divider/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Divider
+//
+//  Created by Jake Convery on 9/14/20.
+//  Copyright © 2020 Jake Convery. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Divider/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Divider
+//
+//  Created by Jake Convery on 9/14/20.
+//  Copyright © 2020 Jake Convery. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
